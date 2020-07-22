238e8f06eb7e4634b5ae25cad8bdce789e2f6ecc
Carlos Caro <carlosjesuscaro@icloud.com>
2020-07-22 17:22:47 -0400

    Class 3

--- class 2.R
+++ class 2.R
@@ -3,12 +3,9 @@
 # Created by: carloscaro
 # Created on: 2020-07-16
 
-
-
   # One sample
 library(survival)
 library(tidyverse)
-
 
 ## Entering right-censored data in R
 
@@ -28,13 +25,14 @@ plot(fit.KM, mark.time = TRUE,
 # Question: what is the median survival time?
 fit.KM
 
-
 ## Nelson-AAlen estimator
 fit.NA <- survfit(Surv(time, status) ~ 1, data = dat, type = "fh")
 summary(fit.NA)
 
 fit.NA
 
+###########################################################################################
+###########################################################################################
 ## Case study: the Xelox trial
 
 library(survival)
@@ -49,6 +47,7 @@ summary(dat)
 
 # How the Progress Free Survival times data looks like (ignoring censoring info)?
 hist(dat$timeWeeks * 7 / 365.25)
+# No conclusion should be driven from this histogram, any conclusion would be incorrect
 
 ### Check censoring coding
 with(dat, Surv(timeWeeks, delta))
@@ -70,12 +69,14 @@ plot(fit.KM)
 # Question: what is the median survival time?
 fit.KM
 
-
 # Note that the definition of censoring depends on what's the quantity of interest.
 # If we're interested in measuring the follow-up time, delta is to be 'inverted':
 dat <- mutate(dat, delta_followUp = 1 - delta)
 fit.followUp <- survfit(Surv(timeYears, delta_followUp) ~ 1, data = dat, conf.type = "log-log")
 fit.followUp
+
+###########################################################################################
+###########################################################################################
 
 # Nonparametric comparison of two samples
 ## Entering right-censored data in R
@@ -87,6 +88,9 @@ dat <- data.frame(ratID = paste0("rat", 1:5),
 ## The logrank test
 fit.logrank <- survdiff(Surv(time, status) ~ group, data = dat)
 fit.logrank
+
+###########################################################################################
+###########################################################################################
 
 ## Case study: the pancreatic dataset
 library(asaur)
@@ -118,6 +122,8 @@ dat <- mutate(dat,
   OS = difftime(death, onstudy, units = "days"),
   PFS = ifelse(!is.na(progression), difftime(progression, onstudy, units = "days"), OS)
 )
+# OS: overall survival
+# PFS: progression free survival
 
 # Note: OS and PFS are expressed in days. We want them in months:
 
@@ -125,7 +131,6 @@ dat <- mutate(dat,
   OS = as.numeric(OS) / 30.5,
   PFS = as.numeric(PFS) / 30.5
 )
-
 
 ### compare PFS in the 2 disease groups
 # As we have no censoring, we can produce use simple boxplots:
@@ -145,16 +150,19 @@ fit.KM
 ### The logrank test
 survdiff(Surv(PFS) ~ stage, data = dat)
 
-
 # What's the estimated probability of not experiencing a cancer progression for (at least) 1 year?
 summary(fit.KM, time = 12)
-
 
 # It is similar in the 2 groups, namely between 13% and 15%.
 # Said otherwise, chances are high that the cancer is going to make a comeback
 # within one year.
 
 # Can you repeat the analysis above, this time for OS?
+
+
+###########################################################################################
+###########################################################################################
+
 
 ## Stratified logrank test: pharmacoSmoking dataset
 
@@ -164,7 +172,17 @@ head(dat)
 
 summary(dat)
 
+## Kaplan-Meyer estimator
+fit.KM <- survfit(Surv(ttr,relapse)~grp, data=dat)
+fit.KM
+plot(fit.KM, col=1:2)
+# white line is the combination therapy
+# red line is the patch only therapy
+
+
 # Question: do the 2 treatment group differ significantly in terms of survival to relapse?
+# calculating the p-value with the Logrank test
+# p-value is very small so it is very significant
 survdiff(Surv(ttr, relapse) ~ grp, data = dat)
 
 # Critique: the 2 groups have different age distribution, which might confound our results.
@@ -175,7 +193,9 @@ with(dat, mosaicplot(table(grp, ageGroup2)))
 
 ### stratified logrank test
 survdiff(Surv(ttr, relapse) ~ grp + strata(ageGroup2), data = dat)
-
+# based on the p-value (0.8%), we are still confident that on the relevance of the
+# experiment results. The strata command essentially performs the test for the 2 different age
+# groups and then combines the results into only one p-value
 
 ### extra
 fit.4 <- survfit(Surv(ttr, relapse) ~ grp + employment, data = dat)
---
+++ class 3.R
@@ -0,0 +1,173 @@
+# Title     : "Cox regression"
+# Objective : TODO
+# Created by: carloscaro
+# Created on: 2020-07-21
+
+# A manually worked out, simple example: two groups
+## Load libraries
+
+library(tidyverse)
+library(maxLik)
+library(survival)
+
+## Data definition
+dat <- data.frame(ratID = paste0("rat", 1:5),
+                  time = c(55, 50, 70, 120, 110),
+                  failure = c(0, 1, 1, 0, 1),
+                  group = c(0, 1, 0, 1, 1))
+
+# Total number of failures D:
+sum(dat$failure)
+
+# For convenience, rename 'group' to 'x':
+dat <- rename(dat, x = group)
+dat
+
+# We also define an auxiliary data.frame containing events only:
+dat.events <- subset(dat, failure == 1)
+
+## Partial log-likelihood function
+
+# Lets define the partial (log-)likelihood function
+pLogLik <- function(beta) {
+  numerator <- with(dat.events, x * beta)
+  denominator <- rep(NA_real_, length(numerator))
+  for(j in seq_along(denominator)) {
+    t_j <- dat.events[j, "time"]
+    risk_set <- subset(dat, time >= t_j)
+    theta_j <- with(risk_set, exp(x * beta))
+    denominator[j] <- log(sum(theta_j))
+  }
+  return(sum(numerator - denominator))
+}
+
+debugonce(pLogLik)
+pLogLik(3)
+
+# We can plot it:
+f <- Vectorize(pLogLik)
+curve(f, from = -4, to = 4)
+
+## Maximum partial-Likelihood estimation
+fit.ML <- maxLik(pLogLik, start = c(beta = 0))
+summary(fit.ML)
+
+# With the `coxph` function:
+fit.cph <- coxph(Surv(time, failure) ~ x, data = dat)
+confint(fit.cph)
+summary(fit.cph)
+
+# We can reproduce the Likelihood-ratio test:
+LRT <- 2 * (fit.ML$maximum - pLogLik(0))
+data.frame(LRT = LRT,
+           pvalue = pchisq(LRT, df = 1, lower.tail = FALSE))
+
+# The Wald test is already in the `maxLik` summary output.
+
+# A manually worked out, simple example: one continuous covariate
+
+dat <- data.frame(time = c(6, 7, 10, 15, 19, 25),
+                  event = c(1, 0, 1, 1, 0, 1),
+                  age = c(67, 62, 34, 41, 46, 28))
+
+fit <- coxph(Surv(time, event) ~ age, data = dat)
+summary(fit)
+
+pred <- survfit(fit, newdata = data.frame(age = c(20, 40, 60)))
+pred
+plot(pred, col = 1:3)
+
+# We might express age in decades:
+dat <- mutate(dat, age_dec = age / 10)
+summary(coxph(Surv(time, event) ~ age_dec, data = dat))
+
+# Case study: the pharmacoSmoking dataset
+
+## Load the data
+library(asaur)
+dat <- pharmacoSmoking
+head(dat)
+
+## Fit the Cox model
+fit <- coxph(Surv(ttr, relapse) ~ grp + age + gender + priorAttempts, data = dat)
+summary(fit)
+
+# We can change the contrasts as we see fit:
+dat <- mutate(dat, grp = relevel(grp, ref = "patchOnly"))
+fit <- update(fit)
+summary(fit)
+
+dat <- mutate(dat, employment = relevel(employment, ref = "pt"))
+fit <- coxph(Surv(ttr, relapse) ~ grp + employment, data = dat)
+summary(fit)
+
+# Case study: pharmakoSmoking, reloaded
+d_raw <- pharmacoSmoking
+
+table(d_raw$employment, useNA = "always")
+
+dat <-
+  mutate(d_raw,
+    employment = ifelse(employment == "ft", "ft", "other"),
+    grp = relevel(grp, ref = "patchOnly"),
+    race = ifelse(race == "white", "white",
+           ifelse(race == "black", "black",
+                  "other")))
+table(dat$employment)
+table(dat$race)
+
+## A. Model fit
+fit <- coxph(Surv(ttr, relapse) ~ grp + age + gender + employment + race, data = dat)
+summary(fit)
+
+broom::tidy(fit) %>%
+  write_csv("coefficients_table.csv")
+
+## B. Data segmentation
+d_new <- select(dat, -ttr, -relapse)
+
+d_segmented <-
+  d_new %>%
+  mutate(risk_score = predict(fit, newdata = d_new, type = "lp"))
+head(d_segmented)
+
+d_segmented %>%
+  arrange(desc(risk_score)) %>%
+  head(10)
+
+## C. Predicting median ttr and Surv(6 months | covariates)
+d0 <- d_new[1,]
+p_S <- survfit(fit, newdata = d0)
+summary(p_S)
+
+plot(p_S)
+
+make_individual_prediction <- function(grp, age, gender, employment, race) {
+  ## LEFT AS AN EXERCISE
+  tibble(median_ttr = ..., surv_6_months = ...)
+}
+
+# Case study: the lung cancer dataset
+
+## Load the data
+library(survival)
+
+dat <- lung
+dat$sex <- factor(dat$sex)
+
+## Nelson-AAlen estimators
+pred.NA <- survfit(Surv(time, status) ~ sex, data = dat, type = "fh")
+plot(pred.NA, col = 1:2)
+
+## Cox regression: predictions
+fit.cph <- coxph(Surv(time, status) ~ sex, data = dat)
+
+pred.cph <- survfit(fit.cph, newdata = data.frame(sex = factor(1:2)),
+                    type = "aalen")
+
+plot(pred.cph, col = 1:2)
+
+# How does the proportional hazards assumption hold?
+plot(pred.NA, fun = "cloglog", col = 1:2)
+
+plot(pred.cph, fun = "cloglog", col = 1:2)
